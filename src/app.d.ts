fd7a6a230511ea28f6be734e98c15bdd70558890
lovelycentury <lovelycenturybeats@gmail.com>
2024-02-06 19:10:07 +0100

    feat: implement qr code component

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,23 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+  interface Window {
+    QRCode: function | object;
+  }
+
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+
+    export interface HTMLAttributes<T> {
+      "on:clickoutside"?: (event: CustomEvent<T>) => void;
+      "on:copysuccess"?: (event: CustomEvent<T>) => void;
+      "on:copyerror"?: (event: CustomEvent<T>) => void;
+    }
+  }
 }
 
 export {};
