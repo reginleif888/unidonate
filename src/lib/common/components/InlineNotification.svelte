11ff900f25d53503044fd87f76a3247c2640b3dc
lovelycentury <lovelycenturybeats@gmail.com>
2024-02-06 19:06:05 +0100

    feat: implement inline notifications

---
+++ src/lib/common/components/InlineNotification.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  export let type: "error" | "success" | "warning" | "info" = "error";
+
+  export let title: string;
+  export let message: string;
+</script>
+
+<div
+  class="inline-notification-root"
+  class:error={type === "error"}
+  class:success={type === "success"}
+  class:warning={type === "warning"}
+  class:info={type === "info"}
+>
+  <span class="subtitle1">{title}</span>
+  <span class="subtitle2">{message}</span>
+</div>
+
+<style>
+  .subtitle1 {
+    font-weight: 700;
+  }
+
+  .inline-notification-root {
+    position: relative;
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    gap: 8px;
+    padding: 8px;
+    border: 1px solid var(--divider-color);
+    background-color: var(--primary-bg);
+    border-color: var(--primary-bg);
+    color: var(--error-color);
+    padding: 16px;
+  }
+
+  .inline-notification-root::before {
+    content: "";
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    bottom: 0px;
+    height: 100%;
+    width: 4px;
+  }
+
+  .error {
+    background-color: var(--error-bg);
+  }
+
+  .error::before {
+    background-color: var(--error-color);
+  }
+
+  .success {
+    background-color: var(--success-bg);
+  }
+
+  .success::before {
+    background-color: var(--success-color);
+  }
+
+  .warning {
+    background-color: var(--warning-bg);
+  }
+
+  .warning::before {
+    background-color: var(--warning-color);
+  }
+
+  .info {
+    background-color: var(--info-bg);
+  }
+
+  .info::before {
+    background-color: var(--info-color);
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
-  import Button from "$lib/common/components/Button.svelte";
 </script>
 
 <svelte:head>
   <title>Vite + SvelteKit + Motoko</title>
 </svelte:head>
 
-<div>
-  Hello World!
-
-  <Button label="hello" />
-</div>
+<div>Hello World!</div>
